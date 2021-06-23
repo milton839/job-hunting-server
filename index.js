2ca3b2353e8baf2a55c50aa2a6312514ab9244da
Milton839 <miltonbiswas839@gmail.com>
2021-06-23 06:16:12 +0600

    dfdf

--- index.js
+++ index.js
@@ -138,12 +138,12 @@ client.connect(err => {
     })
   })
 
-  //  app.get('/candidateListByEmail',(req,res) => {
-  //   candidateCollection.find({email:req.query.email})
-  //   .toArray((err, documents) => {
-  //     res.send(documents);
-  //   })
-  // })
+   app.get('/jobListByEmail',(req,res) => {
+    jobCollection.find({email:req.query.email})
+    .toArray((err, documents) => {
+      res.send(documents);
+    })
+  })
 
 
   app.patch('/update/:id',(req,res) => {
